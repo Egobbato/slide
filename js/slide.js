b35b6228c11c0064153a6500d059a87d8a3125dc
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2024-09-22 11:01:04 -0300

    Slide Atualizar Distâncias

--- js/slide.js
+++ js/slide.js
@@ -2,18 +2,39 @@ export default class Silde {
   constructor(display, slide) {
     this.slide = document.querySelector(slide);
     this.display = document.querySelector(display);
+    this.dist = {
+      finalPosition: 0,
+      startX: 0,
+      movement: 0,
+    };
+  }
+
+  moveSlide(distX) {
+    this.dist.movePosition = distX;
+    this.display.style.transform = `translate3d(${distX}px,0,0)`;
+  }
+
+  updatePosition(clientX) {
+    this.dist.movement = (this.dist.startX - clientX) * 1.6;
+    return this.dist.finalPosition - this.dist.movement;
   }
 
   onStart(event) {
     event.preventDefault();
+
+    this.dist.startX = event.clientX;
+
     this.display.addEventListener("mousemove", this.onMove);
   }
 
-  onMove(event) {}
+  onMove(event) {
+    const finalPosition = this.updatePosition(event.clientX);
+    this.moveSlide(finalPosition);
+  }
 
   onEnd(event) {
-    console.log("acabou");
     this.display.removeEventListener("mousemove", this.onMove);
+    this.dist.finalPosition = this.dist.movePosition;
   }
 
   addSlideEvents() {
